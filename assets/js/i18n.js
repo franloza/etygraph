279b986cf40184ad3659aa35570afc56c56e3d51
Fran Lozano <francisco.lozano@netlify.com>
2021-01-05 12:01:08 +0100

    Add french

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,36 +1,54 @@
 const messages = {
     en: {
         message: {
-        search: 'Search',
-        language: 'Language',
-        english: 'English',
-        spanish: 'Spanish',
-        no_results: 'No results'
+            search: 'Search',
+            language: 'Language',
+            english: 'English',
+            spanish: 'Spanish',
+            french: 'French',
+            no_results: 'No results'
         }
     },
     es: {
         message: {
-        search: 'Buscar',
-        language: 'Idioma',
-        english: 'Inglés',
-        spanish: 'Español',
-        no_results: 'Sin resultados'
+            search: 'Buscar',
+            language: 'Idioma',
+            english: 'Inglés',
+            spanish: 'Español',
+            french: "Francés",
+            no_results: 'Sin resultados'
+        }
+    },
+    fr: {
+        message: {
+            search: 'Chercher',
+            language: 'Langue',
+            english: 'Anglaise',
+            spanish: 'Espagnole',
+            french: 'Français',
+            no_results: 'Aucun résultat'
         }
     }
 }
 
 export const i18n = new VueI18n({
-    locale: 'en', 
     messages,
 })
 
-
-export const locale_to_flag = {
-    'en': 'gb',
-    'es': 'es'
-}
-
-export const locale_to_lang = {
-'en': 'eng',
-'es': 'spa'
+export const locale_data = {
+    en: {
+        flag: 'gb',
+        lang: 'eng',
+        name: 'english'
+    }, 
+    es:  {
+        flag: 'es',
+        lang: 'spa',
+        name: 'spanish'
+    }, 
+    fr:  {
+        flag: 'fr',
+        lang: 'fra',
+        name: 'french'
+    }, 
 }
